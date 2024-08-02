e7219cf3379ab352a9f473bdcd8ec2afd6c474cf
Nikita Ivanov <ivanow.1994@gmail.com>
2024-08-03 01:23:43 +0500

    update project

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.app")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true)}
+}
+
+
+
---
+++ app/src/main/java/hexlet/code/game/Prime.java
@@ -0,0 +1,40 @@
+package hexlet.code.game;
+
+import hexlet.code.Engine;
+
+public class Prime {
+    static final String RULES_GAME = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+    static final int NUMBER_RANGE = 100;
+    private static String[][] data = new String[3][2];
+
+    public Prime() {
+    }
+
+    public static void game() {
+        Engine.start(RULES_GAME);
+
+        for (int i = 0; i < 3; i++) {
+            int randomNum = (int) (Math.random() * NUMBER_RANGE);
+            data[i][0] = question(randomNum);
+            data[i][1] = isPrime(randomNum);
+        }
+
+        Engine.resultGame(data);
+    }
+
+    public static String question(int number) {
+        return "Question: " + number;
+    }
+
+    public static String isPrime(int number) {
+        int countDevider = 1;
+
+        for (int i = 2; i <= number; ++i) {
+            if (number % i == 0) {
+                ++countDevider;
+            }
+        }
+
+        return countDevider == 2 ? "yes" : "no";
+    }
+}
