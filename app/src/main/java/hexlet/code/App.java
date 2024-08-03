91db385fc230279042c42e0f5c93c9e050b9ee0d
Nikita Ivanov <ivanow.1994@gmail.com>
2024-08-03 22:12:21 +0500

    update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,7 +14,7 @@ public class App {
     private static String choice;
 
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter. \n1 - Greet\n2 - Even"
+        System.out.println("Please enter the game number and press Enter.\n1 - Greet\n2 - Even"
                 + "\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit");
         System.out.print("You choice: ");
         Scanner choiceStr = new Scanner(System.in);
