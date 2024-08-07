53141d2954dda77644744924e8a73f0c4caf0af1
Nikita Ivanov <ivanow.1994@gmail.com>
2024-08-07 22:18:23 +0500

    Code modernization

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -3,7 +3,8 @@ package hexlet.code;
 public class Utils {
     public static int randomNum() {
         int randomNum;
-        randomNum = (int) (Math.random() * 20);
+        int NUMBER_RANGE = 20;
+        randomNum = (int) (Math.random() * NUMBER_RANGE);
         return randomNum;
     }
     public static int randomNum(int number) {
